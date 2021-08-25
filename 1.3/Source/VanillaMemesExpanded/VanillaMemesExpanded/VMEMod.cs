21b1774c82cbccbb7ad3aeda60bacb2e14ba51fb
Joshua <legodude17@users.noreply.github.com>
2021-08-24 19:05:55 -0700

    Fix for rimworld update

--- 1.3/Source/VanillaMemesExpanded/VanillaMemesExpanded/VMEMod.cs
+++ 1.3/Source/VanillaMemesExpanded/VanillaMemesExpanded/VMEMod.cs
@@ -23,7 +23,7 @@ namespace VanillaMemesExpanded
             var idx1 = list.FindIndex(ins => ins.opcode == OpCodes.Ble_S);
             var labels = new List<Label>();
             foreach (var ins in list.GetRange(idx1 - 2, 9)) labels.AddRange(ins.labels);
-            list.RemoveRange(idx1 - 2, 7);
+            list.RemoveRange(idx1 - 2, 9);
             list[idx1].labels.AddRange(labels);
             return list;
         }
