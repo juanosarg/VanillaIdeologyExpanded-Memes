7d8c96b6cbf99ac415a8d4ac75492022429f5c69
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-09-18 19:57:09 +0200

    v0.1

---
+++ 1.3/Source/VanillaMemesExpanded/VanillaMemesExpanded/Thoughts/ThoughtWorker_Precept_Leather_Abhorrent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Verse;
+using RimWorld;
+using System.Collections.Generic;
+
+namespace VanillaMemesExpanded
+{
+	public class ThoughtWorker_Precept_Leather_Abhorrent : ThoughtWorker_Precept
+	{
+		protected override ThoughtState ShouldHaveThought(Pawn p)
+		{
+			bool flag = false;
+			List<Apparel> wornApparel = p.apparel.WornApparel;
+			for (int i = 0; i < wornApparel.Count; i++)
+			{
+				if (wornApparel[i].Stuff?.defName.Contains("Leather")==true)
+				{
+					flag = true;
+				}
+			}
+
+			if (flag)
+			{
+
+				return ThoughtState.ActiveAtStage(0);
+			}
+			else return false;
+
+
+
+
+
+
+		}
+	}
+}
