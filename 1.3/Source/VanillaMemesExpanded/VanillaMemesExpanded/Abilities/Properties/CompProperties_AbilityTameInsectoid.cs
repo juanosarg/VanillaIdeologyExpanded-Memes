e056e55e41b444fba59ce3a7e8b3b3f12b2ebf0a
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-09-18 16:33:47 +0200

    v0.1

---
+++ 1.3/Source/VanillaMemesExpanded/VanillaMemesExpanded/Abilities/Properties/CompProperties_AbilityTameInsectoid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Verse;
+using RimWorld;
+
+namespace VanillaMemesExpanded
+{
+	public class CompProperties_AbilityTameInsectoid : CompProperties_AbilityEffect
+	{
+		public CompProperties_AbilityTameInsectoid()
+		{
+			this.compClass = typeof(CompAbilityTameInsectoid);
+		}
+
+		
+	}
+}
---
+++ 1.3/Source/VanillaMemesExpanded/VanillaMemesExpanded/Rituals/RitualOutcomeEffectWorker_Bonfire.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Verse;
+using RimWorld;
+
+namespace VanillaMemesExpanded
+{
+	public class RitualOutcomeEffectWorker_Bonfire : RitualOutcomeEffectWorker_FromQuality
+	{
+		public override bool SupportsAttachableOutcomeEffect
+		{
+			get
+			{
+				return false;
+			}
+		}
+
+		public RitualOutcomeEffectWorker_Bonfire()
+		{
+		}
+
+		public RitualOutcomeEffectWorker_Bonfire(RitualOutcomeEffectDef def) : base(def)
+		{
+		}
+
+		public override void Apply(float progress, Dictionary<Pawn, int> totalPresence, LordJob_Ritual jobRitual)
+		{
+			float quality = base.GetQuality(jobRitual, progress);
+			OutcomeChance outcome = this.GetOutcome(quality, jobRitual);
+			LookTargets lookTargets = jobRitual.selectedTarget;
+			string text = null;
+			if (jobRitual.Ritual != null)
+			{
+				this.ApplyAttachableOutcome(totalPresence, jobRitual, outcome, out text, ref lookTargets);
+			}
+			bool flag = false;
+			foreach (Pawn pawn in totalPresence.Keys)
+			{
+				
+					base.GiveMemoryToPawn(pawn, outcome.memory, jobRitual);
+				
+
+			}
+
+
+
+			
+
+
+
+
+
+
+
+			string text2 = outcome.description.Formatted(jobRitual.Ritual.Label).CapitalizeFirst() + "\n\n" + this.OutcomeQualityBreakdownDesc(quality, progress, jobRitual);
+			string text3 = this.def.OutcomeMoodBreakdown(outcome);
+			if (!text3.NullOrEmpty())
+			{
+				text2 = text2 + "\n\n" + text3;
+			}
+			if (flag)
+			{
+				text2 += "\n\n" + "RitualOutcomeExtraDesc_Execution".Translate();
+			}
+			if (text != null)
+			{
+				text2 = text2 + "\n\n" + text;
+			}
+			string text4;
+			this.ApplyDevelopmentPoints(jobRitual.Ritual, outcome, out text4);
+			if (text4 != null)
+			{
+				text2 = text2 + "\n\n" + text4;
+			}
+			Find.LetterStack.ReceiveLetter("OutcomeLetterLabel".Translate(outcome.label.Named("OUTCOMELABEL"), jobRitual.Ritual.Label.Named("RITUALLABEL")), text2, outcome.Positive ? LetterDefOf.RitualOutcomePositive : LetterDefOf.RitualOutcomeNegative, lookTargets, null, null, null, null);
+			
+
+		}
+	}
+}
