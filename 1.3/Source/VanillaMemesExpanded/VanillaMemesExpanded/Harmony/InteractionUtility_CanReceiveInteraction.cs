b3e66c2bcab8bacdb7757f3a564cae152dcfdd1f
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-09-30 08:34:58 +0200

    v0.1

---
+++ 1.3/Source/VanillaMemesExpanded/VanillaMemesExpanded/Harmony/InteractionUtility_CanReceiveInteraction.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using Verse.AI;
+
+
+
+namespace VanillaMemesExpanded
+{
+
+
+    [HarmonyPatch(typeof(InteractionUtility))]
+    [HarmonyPatch("CanReceiveInteraction")]
+    public static class VanillaMemesExpanded_InteractionUtility_CanReceiveInteraction_Patch
+    {
+        [HarmonyPostfix]
+        static void DontInteract(Pawn pawn, ref bool __result)
+        {
+           if(pawn.Ideo?.HasPrecept(InternalDefOf.VME_SocialInteractions_Disallowed) == true)
+            {
+                __result = false;
+            }
+
+        }
+    }
+
+
+
+
+
+
+
+
+}
---
+++ 1.3/Source/VanillaMemesExpanded/VanillaMemesExpanded/Harmony/JoyUtility_JoyTickCheckEnd.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using Verse.AI;
+
+
+
+namespace VanillaMemesExpanded
+{
+
+
+    [HarmonyPatch(typeof(JoyUtility))]
+    [HarmonyPatch("JoyTickCheckEnd")]
+    public static class VanillaMemesExpanded_JoyUtility_JoyTickCheckEnd_Patch
+    {
+        [HarmonyPostfix]
+        static void InformJoyTakingPlace(Pawn pawn)
+        {
+            if (pawn.IsHashIntervalTick(2000)) {
+                Find.HistoryEventsManager.RecordEvent(new HistoryEvent(InternalDefOf.VME_HavingFun, pawn.Named(HistoryEventArgsNames.Doer)), true);
+            }
+
+
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+}
