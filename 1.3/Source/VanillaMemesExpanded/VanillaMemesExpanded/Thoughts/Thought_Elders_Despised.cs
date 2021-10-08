ea0f19b0bfd5583e7782f9e5d636f922b4d53a2b
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-10-08 16:54:01 +0200

    v0.2

---
+++ 1.3/Source/VanillaMemesExpanded/VanillaMemesExpanded/Thoughts/Thought_Elders_Despised.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace VanillaMemesExpanded
+{
+	public class Thought_Elders_Despised : ThoughtWorker_Precept_Social
+	{
+		protected override ThoughtState ShouldHaveThought(Pawn p, Pawn otherPawn)
+		{
+			if (otherPawn.ageTracker.AgeBiologicalYears >= 65)
+			{
+				return ThoughtState.ActiveAtStage(0);
+			}
+
+			if (otherPawn.ageTracker.AgeBiologicalYears <= 25)
+			{
+				return ThoughtState.ActiveAtStage(1);
+			}
+
+			else return false;
+
+
+		}
+
+
+	}
+}
