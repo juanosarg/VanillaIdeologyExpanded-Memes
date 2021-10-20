3d9f3c7b74dc25082db483b4a44fcd4625b7a1d2
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-10-20 21:48:47 +0200

    v0.37

---
+++ 1.3/Source/VanillaMemesExpanded/VanillaMemesExpanded/Harmony/IdeoUIUtility_DoPrecepts.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Reflection.Emit;
+
+
+
+namespace VanillaMemesExpanded
+{
+
+
+    [HarmonyPatch(typeof(IdeoUIUtility))]
+    [HarmonyPatch("DoPrecepts")]
+    public static class VanillaMemesExpanded_IdeoUIUtility_DoPrecepts_Patch
+    {
+        [HarmonyPostfix]
+        static void EnableMorePrecepts()
+        {
+            List<CurvePoint> newCurve = new List<CurvePoint>();
+            newCurve.Add(new CurvePoint(0.5f, 2));
+            newCurve.Add(new CurvePoint(1f, 10));
+            DefDatabase<PreceptDef>.GetNamedSilentFail("IdeoRelic").preceptInstanceCountCurve.Points.Clear();
+            DefDatabase<PreceptDef>.GetNamedSilentFail("IdeoRelic").preceptInstanceCountCurve.Points.AddRange(newCurve);
+
+            List<CurvePoint> newCurve2 = new List<CurvePoint>();
+            newCurve2.Add(new CurvePoint(0.5f, 2));
+            newCurve2.Add(new CurvePoint(1f, 10));
+            DefDatabase<PreceptDef>.GetNamedSilentFail("IdeoBuilding").preceptInstanceCountCurve.Points.Clear();
+            DefDatabase<PreceptDef>.GetNamedSilentFail("IdeoBuilding").preceptInstanceCountCurve.Points.AddRange(newCurve2);
+        }
+    }
+}
---
+++ 1.3/Source/VanillaMemesExpanded/VanillaMemesExpanded/Harmony/IdeoUIUtility_DoStyles.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Reflection.Emit;
+
+
+
+namespace VanillaMemesExpanded
+{
+
+
+    [HarmonyPatch(typeof(IdeoUIUtility))]
+    [HarmonyPatch("DoStyles")]
+    public static class VanillaMemesExpanded_IdeoUIUtility_DoStyles_Patch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+
+
+            var codes = instructions.ToList();
+
+            for (var i = 0; i < codes.Count; i++)
+            {
+
+                var code = codes[i];
+                if (i > 0 && codes[i - 1].opcode == OpCodes.Ldloc_1 && codes[i].opcode == OpCodes.Ldc_I4_3)
+                {
+
+                    yield return new CodeInstruction(OpCodes.Ldc_I4, 5);
+                }
+                
+                else
+                {
+                    yield return code;
+                }
+
+
+            }
+
+
+        }
+    }
+}
