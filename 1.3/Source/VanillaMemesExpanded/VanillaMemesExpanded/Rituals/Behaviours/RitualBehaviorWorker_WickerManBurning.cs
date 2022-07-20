93eb6f26855bf56ee784a88c2a5a2ebb52fafc6f
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-07-20 08:31:23 +0200

    v1.26

--- 1.3/Source/VanillaMemesExpanded/VanillaMemesExpanded/Rituals/Behaviours/RitualBehaviorWorker_WickerManBurning.cs
+++ 1.3/Source/VanillaMemesExpanded/VanillaMemesExpanded/Rituals/Behaviours/RitualBehaviorWorker_WickerManBurning.cs
@@ -19,27 +19,31 @@ namespace VanillaMemesExpanded
 
 		public override string CanStartRitualNow(TargetInfo target, Precept_Ritual ritual, Pawn selectedPawn = null, Dictionary<string, Pawn> forcedForRole = null)
 		{
-			Precept_Role precept_Role = ritual.ideo.RolesListForReading.First((Precept_Role r) => r.def == InternalDefOf.VME_IdeoRole_FireKeeper);
-			if (precept_Role.ChosenPawnSingle() == null)
-			{
-				return "CantStartRitualRoleNotAssigned".Translate(precept_Role.LabelCap);
-			}
-			bool flag = false;
-			List<Pawn> listOfPawns = target.Map.mapPawns.FreeColonistsAndPrisonersSpawned;
-			foreach(Pawn pawn in listOfPawns)
+			Precept_Role precept_Role = ritual.ideo.RolesListForReading.FirstOrDefault((Precept_Role r) => r.def == InternalDefOf.VME_IdeoRole_FireKeeper);
+			if(precept_Role != null)
             {
-				if (ValidateConvertee(pawn, precept_Role.ChosenPawnSingle(), false))
+				if (precept_Role.ChosenPawnSingle() == null)
 				{
-					flag = true;
-					break;
+					return "CantStartRitualRoleNotAssigned".Translate(precept_Role.LabelCap);
+				}
+				bool flag = false;
+				List<Pawn> listOfPawns = target.Map.mapPawns.FreeColonistsAndPrisonersSpawned;
+				foreach (Pawn pawn in listOfPawns)
+				{
+					if (ValidateConvertee(pawn, precept_Role.ChosenPawnSingle(), false))
+					{
+						flag = true;
+						break;
+					}
+				}
+
+
+				if (!flag)
+				{
+					return "VME_CantStartWickerManRitual".Translate(precept_Role.ChosenPawnSingle().Ideo.name);
 				}
 			}
-
 			
-			if (!flag)
-			{
-				return "VME_CantStartWickerManRitual".Translate(precept_Role.ChosenPawnSingle().Ideo.name);
-			}
 			return base.CanStartRitualNow(target, ritual, selectedPawn, forcedForRole);
 		}
 
